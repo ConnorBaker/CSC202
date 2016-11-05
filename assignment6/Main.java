f096a0b542a732cf3ac34f2986e84a3f1fe5da34
Connor Baker <ConnorBaker01@gmail.com>
2016-11-05 18:57:40 -0400

    assignment6: Push version 0.3a

--- assignment6/Main.java
+++ assignment6/Main.java
@@ -3,64 +3,53 @@
 *  File Name: Main.java
 *
 *  Author: Connor Baker
-*  Version: 0.2a
+*  Version: 0.3a
 *  Created: October 31, 2016
-*  Last Updated: November 2, 2016
-*
-*  Summary of problem specification:
-*  Using a standard tuning frequency of 16.7MHZ, find the inducance of a
-*  circuit using formulae given in class (see formulae section). After find the
-*  inductance, input it into the formula given to find frequency minimum and
-*  frequency maximum, recording the values of both. Then, increment the
-*  capacitance by 15 picofarad, recording the new value of frequency minimum
-*  for each increment, until we meet frequency maximum, writing all these
-*  values to a random access file.
-*
-*  Functionality:
-*  Working on it.
-*
-*  Explanation of Components:
-*  See comments.
-*
-*  Formulae Used:
-*  inductance = ((2pi/frequency)^2)/capacitance
-*  capacitance = sqrt(capacitanceMinimum*capacitanceMaximum)
-*  frequencyMinimum = (2pi)/sqrt(inductance*capacitanceMaximum)
-*  frequencyMaximum = (2pi)/sqrt(inductance*capacitanceMinimum)
-*
-*  Assumptions:
-*  Java EA 9 didn't allow me to write anything that isn't backwards compliant
-*  with the current version.
-*
-*  References:
-*  http://download.java.net/java/jdk9/docs/api/
+*  Last Updated: November 5, 2016
 */
 
 // Declare our package
 package assignment6;
 
-public class Main {
-  public static void main(String args[]) {
+public class Main { // begin class Main
+  public static void main(String args[]) { // begin main()
+    // Create our object
     TuningCircuit circuit = new TuningCircuit();
 
-    /*
-    *  TODO: Have the while loop keep only the greatest value of fMax. We know
-    *  (thanks to the formula for frequencyMaximum) that if the next value of
-    *  fMax is lower, we can halt calculation, because every value thereafter
-    *  will be lower as well (since we're gradually increasing the value of the
-    *  denominator as we increment capacitanceMinimum).
-    */
-    // Calculuate the values of fMax for a given capacitance
-    while (circuit.cMin < circuit.cMax) {
-      // Increment cMin by 15 picofarad
-      circuit.cMin = circuit.cMin + 0.000000000015;
-      // Get the new value of fMax
-      circuit.getfMax();
-      // Print the new value of fMax
-      System.out.println("new fMax is "+circuit.fMax);
-    }
-    // Reset cMin to it's default value
-    circuit.cMin = 0.000000000015;
+    // Run processing on our object
+    while (true) { // begin outer while
+      // Condition to break out of while loop
+      if (circuit.fMax < circuit.frequency) { //begin if
+        /*
+        *  The value given for inductance is great enough that all frequency
+        *  calculated will be below the standard tuning frequency for any given
+        *  capacitance within our range of 15-365 picofarad.
+        */
+        break;
+      } // end if
 
-  }
-}
+      // Calculuate the values of fMax for a given capacitance
+      while (circuit.cMin < circuit.cMax) { // begin inner while
+        // Increment cMin by 15 picofarad
+        circuit.cMin += 0.000000000015;
+
+        // Get the new value of fMax
+        circuit.getfMax();
+
+        // If fMax is less than 16.7MHZ terminate the program
+        if (circuit.fMax > circuit.frequency) { // begin if
+          // Print the new value of fMax
+          System.out.println("new fMax is "+circuit.fMax);
+        } else {
+          // Reset the cMin of our object and halt
+          circuit.cMin = 0.000000000015;
+          circuit.getfMax();
+          break;
+        } // end if
+      } // end inner while
+
+      // If the halt condition is not yet met, increment and run again
+      circuit.inductance *= 1.02;
+    } // end outer while
+  } // end main()
+} // end class Main
--- assignment6/TuningCircuit.java
+++ assignment6/TuningCircuit.java
@@ -3,18 +3,23 @@
 *  File Name: TuningCircuit.java
 *
 *  Author: Connor Baker
-*  Version: 0.2a
+*  Version: 0.3a
 *  Created: October 31, 2016
-*  Last Updated: November 2, 2016
+*  Last Updated: November 5, 2016
 */
 
 // Declare our package
 package assignment6;
 
-public class TuningCircuit {
+public class TuningCircuit { // begin class TuningCircuit
   // cMin & cMax, fMin & fMax are capacitance and frequency bounds, respectively
   double inductance, capacitance, cMin, cMax, frequency, fMin, fMax;
 
+  // Variables which we use frequently should be pre-computed
+  final double fourPiSquared = 4*Math.PI*Math.PI;
+  final double twoPi = 2*Math.PI;
+
+  // Default constructor for our object
   public TuningCircuit() {
     frequency = 16700000;
     cMin = 0.000000000015;
@@ -25,19 +30,27 @@ public class TuningCircuit {
     getfMax();
   }
 
+  // Our method to calculate the capacitance of our circuit
   public void getCapacitance() {
+    // See equation (2)
     capacitance = Math.sqrt(cMin * cMax);
   }
 
+  // Our method to calculate the inductance of our circuit
   public void getInductance() {
-    inductance = (((4*Math.PI*Math.PI)/(frequency*frequency))/capacitance);
+    // See equation (1)
+    inductance = ((fourPiSquared/(frequency*frequency))/capacitance);
   }
 
+  // Our method to calculate the frequencyMinimum of our circuit
   public void getfMin() {
-    fMin = ((2*Math.PI)/Math.sqrt(inductance*cMax));
+    // See equation (3)
+    fMin = (twoPi/Math.sqrt(inductance*cMax));
   }
 
+  // Our method to calculate the frequencyMaximum of our circuit
   public void getfMax() {
-    fMax = ((2*Math.PI)/Math.sqrt(inductance*cMin));
+    // See equation (4)
+    fMax = (twoPi/Math.sqrt(inductance*cMin));
   }
-}
+} // end class TuningCircuit

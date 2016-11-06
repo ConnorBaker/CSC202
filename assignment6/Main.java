0ba64aee6dca049e8fe0d50c85fab9ce4300b2d2
Connor Baker <ConnorBaker01@gmail.com>
2016-11-06 16:15:48 -0500

    assignment6: Push version 0.4a

--- assignment6/Main.java
+++ assignment6/Main.java
@@ -3,27 +3,40 @@
 *  File Name: Main.java
 *
 *  Author: Connor Baker
-*  Version: 0.3a
+*  Version: 0.4a
 *  Created: October 31, 2016
-*  Last Updated: November 5, 2016
+*  Last Updated: November 6, 2016
 */
 
 // Declare our package
 package assignment6;
 
+// Import packages
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+
 public class Main { // begin class Main
-  public static void main(String args[]) { // begin main()
+  public static void main(String args[]) throws IOException { // begin main method
     // Create our object
     TuningCircuit circuit = new TuningCircuit();
+
+    // Create our output stream
+    File outputFile = new File("frequency.txt");
+    RandomAccessFile raf = new RandomAccessFile(outputFile, "rw");
 
     // Run processing on our object
     while (true) { // begin outer while
       // Condition to break out of while loop
-      if (circuit.fMax < circuit.frequency) { //begin if
+      if (circuit.fMax < circuit.standardFrequency) { //begin if
         /*
         *  The value given for inductance is great enough that all frequency
         *  calculated will be below the standard tuning frequency for any given
-        *  capacitance within our range of 15-365 picofarad.
+        *  capacitance within a range of any capacitance, due to calculatefMax
+        *  being a strictly monotonic decreasing function (given inductance and
+        *  capacitance minimum are strictly monotonic increasing functions,
+        *  which they are).
         */
         break;
       } // end if
@@ -34,16 +47,19 @@ public class Main { // begin class Main
         circuit.cMin += 0.000000000015;
 
         // Get the new value of fMax
-        circuit.getfMax();
+        circuit.calculatefMax();
 
         // If fMax is less than 16.7MHZ terminate the program
-        if (circuit.fMax > circuit.frequency) { // begin if
+        if (circuit.fMax > circuit.standardFrequency) { // begin if
           // Print the new value of fMax
-          System.out.println("new fMax is "+circuit.fMax);
+          System.out.println("new fMax is " + circuit.fMax + " hertz. Add to file.");
+          raf.writeDouble(circuit.fMax);
         } else {
           // Reset the cMin of our object and halt
+          System.out.println("new fMax is "+circuit.fMax+" hertz. Don't add to"
+              + " file because it is smaller than " + circuit.standardFrequency + " hertz. Halt calculations and increment inductance by 2%.");
           circuit.cMin = 0.000000000015;
-          circuit.getfMax();
+          circuit.calculatefMax();
           break;
         } // end if
       } // end inner while
@@ -51,5 +67,7 @@ public class Main { // begin class Main
       // If the halt condition is not yet met, increment and run again
       circuit.inductance *= 1.02;
     } // end outer while
-  } // end main()
+
+    raf.close();
+  } // end main method
 } // end class Main
--- assignment6/TuningCircuit.java
+++ assignment6/TuningCircuit.java
@@ -3,53 +3,80 @@
 *  File Name: TuningCircuit.java
 *
 *  Author: Connor Baker
-*  Version: 0.3a
+*  Version: 0.4a
 *  Created: October 31, 2016
-*  Last Updated: November 5, 2016
+*  Last Updated: November 6, 2016
 */
 
 // Declare our package
 package assignment6;
 
+// Import packages
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
 public class TuningCircuit { // begin class TuningCircuit
   // cMin & cMax, fMin & fMax are capacitance and frequency bounds, respectively
-  double inductance, capacitance, cMin, cMax, frequency, fMin, fMax;
+  double inductance, capacitance, cMin, cMax, standardFrequency, fMin, fMax;
 
   // Variables which we use frequently should be pre-computed
   final double fourPiSquared = 4*Math.PI*Math.PI;
   final double twoPi = 2*Math.PI;
 
+  // Set up objects to grab input from the user
+  InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+  BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
   // Default constructor for our object
-  public TuningCircuit() {
-    frequency = 16700000;
-    cMin = 0.000000000015;
-    cMax = 0.000000000365;
-    getCapacitance();
-    getInductance();
-    getfMin();
-    getfMax();
+  public TuningCircuit() throws IOException {
+    promptUserForStandardFrequency();
+    promptUserForCapacitanceMinimum();
+    promptUserForCapacitanceMaximum();
+    calculateCapacitance();
+    calculateInductance();
+    calculatefMin();
+    calculatefMax();
+  }
+
+  // Our method to request the standard frequency from the user
+  public void promptUserForStandardFrequency() throws IOException {
+    System.out.println("Input the standard frequency in hertz:");
+    standardFrequency = Double.parseDouble(bufferedReader.readLine());
+  }
+
+  // Our method to request the lower bound of capacitance from the user
+  public void promptUserForCapacitanceMinimum() throws IOException {
+    System.out.println("Input the lower bound of capacitance in farad:");
+    cMin = Double.parseDouble(bufferedReader.readLine());
+  }
+
+  // Our method to request the upper bound of capacitance from the user
+  public void promptUserForCapacitanceMaximum() throws IOException {
+    System.out.println("Input the upper bound of capacitance in farad:");
+    cMax = Double.parseDouble(bufferedReader.readLine());
   }
 
   // Our method to calculate the capacitance of our circuit
-  public void getCapacitance() {
+  public void calculateCapacitance() {
     // See equation (2)
     capacitance = Math.sqrt(cMin * cMax);
   }
 
   // Our method to calculate the inductance of our circuit
-  public void getInductance() {
+  public void calculateInductance() {
     // See equation (1)
-    inductance = ((fourPiSquared/(frequency*frequency))/capacitance);
+    inductance = ((fourPiSquared/(standardFrequency*standardFrequency))/capacitance);
   }
 
   // Our method to calculate the frequencyMinimum of our circuit
-  public void getfMin() {
+  public void calculatefMin() {
     // See equation (3)
     fMin = (twoPi/Math.sqrt(inductance*cMax));
   }
 
   // Our method to calculate the frequencyMaximum of our circuit
-  public void getfMax() {
+  public void calculatefMax() {
     // See equation (4)
     fMax = (twoPi/Math.sqrt(inductance*cMin));
   }

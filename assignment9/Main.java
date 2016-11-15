11ab9db395245372e9752224d2ea929f7685cc0c
Connor Baker <ConnorBaker01@gmail.com>
2016-11-14 19:34:20 -0500

    assignment9: Push v0.1a

---
+++ assignment9/Main.java
@@ -0,0 +1,22 @@
+/*
+*  Package Name: assignment9
+*  File Name: Main.java
+*
+*  Author: Connor Baker
+*  Version: 0.1a
+*  Created: November 14, 2016
+*  Last Updated: November 14, 2016
+*/
+
+// Declare our package
+package assignment9;
+
+// Import packages
+import java.io.IOException;
+
+public class Main { // begin class Main
+  public static void main(String args[]) throws IOException { // begin main
+    // Create our object
+    Matrix matrix1 = new Matrix();
+  } // end main()
+} // end class Main

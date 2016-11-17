d1c75e34a4862c644b930e1415339a157a711c19
Connor Baker <ConnorBaker01@gmail.com>
2016-11-16 20:01:38 -0500

    groupproject: Matrix: Push v0.1d

--- groupproject/matrices/Matrix.java
+++ groupproject/matrices/Matrix.java
@@ -3,9 +3,18 @@
 *  File Name: Matrix.java
 *
 *  Author: Connor Baker, Rae Bouldin
-*  Version: 0.1c
+*  Version: 0.1d
 *  Created: November 14, 2016
 *  Last Updated: November 16, 2016
+*
+*  A note in general about the array processing methods:
+*  Instead of using array.length and array[i].length in all of the for loops,
+*  I've decided to use the constant 3, because this project deals only with
+*  matrices of the third order. I believe that using a constant instead of
+*  referencing the size of the array (which in of itself requires passing the
+*  array to another method and having java tally the length) provides a
+*  meaningful speedup.
+*
 */
 
 // Declare our package
@@ -34,54 +43,76 @@ public class Matrix { // begin class TuningCircuit
 
   // Default constructor for our object
   public Matrix() throws IOException {
-    fillArray();
+    readArrayFromFile();
   }
 
-  // // Argumented constructor for our object
+  /*
+  *  Currently I can't think of a use for an argumented constructor since we
+  *  read in from files. In what scenario would we need to have user input in
+  *  this program?
+  */
+  // Argumented constructor for our object
   // public Matrix(int array[][], int numOfRows, int numOfCols) throws IOException {
   //
   // }
 
-  public void fillArray() throws IOException {
+  // Method to fill the array from file
+  public void readArrayFromFile() throws IOException {
+    // Initialize our StringTokenizer
     StringTokenizer st;
-    for (int i = 0; i < array.length; i++) {
+    for (int i = 0; i < 3; i++) {
       // We must create a new string tokenizer for each new line we want to read
       st = new StringTokenizer(br.readLine());
-      for (int j = 0; j < array[i].length; j++) {
+      for (int j = 0; j < 3; j++) {
+        // Read the values of the array line by line
         array[i][j] = Integer.parseInt(st.nextToken());
       }
+      // Print out the array
       System.out.println(Arrays.deepToString(array));
     }
   }
 
+  // Method to take the sum of two arrays
   public static void sumOfArrays(int a[][], int b[][]) {
+    // Initialize our resultant array
     int c[][] = new int[3][3];
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
+        // Sum the values of the elements of the two
         c[i][j] = a[i][j] + b[i][j];
       }
     }
+    // Print out the array
     System.out.println(Arrays.deepToString(c));
   }
 
-
+  // Method to take the product of two arrays
   public static void productOfArrays(int a[][], int b[][]) {
+    // Initialize our resultant array
     int c[][] = new int[3][3];
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
+        // Calculates the product using  a formula derived in the documentation
+        // See documentation, section X.X.X for more information
         c[i][j] = a[i][0]*b[0][j] + a[i][1]*b[1][j] + a[i][2]*b[2][j];
       }
     }
+    // Print out the array
     System.out.println(Arrays.deepToString(c));
   }
 
+  // Method to take the transpose of an array
   public static void transposeOfArray(int a[][]) {
+    // Initialize our resultant array
     int c[][] = new int[3][3];
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
+        // Simply swapping the element's position yeilds the transpose
+        // See documentation, section X.X.X for more information
         c[i][j] = a[j][i];
       }
     }
+    // Print out the array
     System.out.println(Arrays.deepToString(c));
   }
 

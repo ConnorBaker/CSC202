8a82b540296c41c6adc85f90d9cf699ae1ee8028
Connor Baker <ConnorBaker01@gmail.com>
2016-11-19 19:42:23 -0500

    groupproject: v0.2b

--- groupproject/matrices/Main.java
+++ groupproject/matrices/Main.java
@@ -3,7 +3,7 @@
 *  File Name: Main.java
 *
 *  Author: Connor Baker, Rae Bouldin
-*  Version: 0.2a
+*  Version: 0.2b
 *  Created: November 14, 2016
 *  Last Updated: November 19, 2016
 */
@@ -23,13 +23,24 @@ import java.io.IOException;
 
 public class Main { // begin class Main
   public static void main(String args[]) throws IOException { // begin main
-    // Create our object
+    // Create our two initial matrices
     Matrix matrix1 = new Matrix();
-    Matrix matrix2 = new Matrix();
-    Matrix.sumOfArrays(matrix1.array, matrix2.array);
-    Matrix.productOfArrays(matrix1.array, matrix2.array);
-    Matrix.transposeOfArray(matrix1.array);
-    Matrix.determinantOfArray(matrix1.array);
-    Matrix.cofactorOfArray(matrix1.array);
-  } // end main()
+    Matrix matrix2 = new Matrix(".\\matrices\\matrix2.txt");
+
+    // Calculate the sum and product of the matrices
+    Matrix.sumOfMatrices(matrix1.matrix, matrix2.matrix);
+    Matrix.productOfMatrices(matrix1.matrix, matrix2.matrix);
+
+    // Calculate the transpose, cofactor, and determinant of the sum matrix
+    Matrix matrix3 = new Matrix(".\\matrices\\sumofmatrices.txt");
+    Matrix.transposeOfMatrix(matrix3.matrix);
+    Matrix.cofactorOfMatrix(matrix3.matrix);
+    Matrix.determinantOfMatrix(matrix3.matrix);
+
+    // Calculate the transpose, cofactor, and determinant of the product matrix
+    Matrix matrix4 = new Matrix(".\\matrices\\productofmatrices.txt");
+    Matrix.transposeOfMatrix(matrix4.matrix);
+    Matrix.cofactorOfMatrix(matrix4.matrix);
+    Matrix.determinantOfMatrix(matrix4.matrix);
+} // end main()
 } // end class Main
--- groupproject/matrices/Matrix.java
+++ groupproject/matrices/Matrix.java
@@ -3,12 +3,12 @@
 *  File Name: Matrix.java
 *
 *  Author: Connor Baker, Rae Bouldin
-*  Version: 0.2a
+*  Version: 0.2b
 *  Created: November 14, 2016
 *  Last Updated: November 19, 2016
 *
-*  A note in general about the array processing methods:
-*  Instead of using array.length and array[i].length in all of the for loops,
+*  A note in general about the matrix processing methods:
+*  Instead of using matrix.length and matrix[i].length in all of the for loops,
 *  I've decided to use the constant 3, because this project deals only with
 *  matrices of the third order. I believe that using a constant instead of
 *  referencing the size of the array (which in of itself requires passing the
@@ -33,8 +33,8 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Matrix { // begin class TuningCircuit
-  // Create our array for processing
-  static int array[][] = new int[3][3];
+  // Create our matrix for processing
+  static int matrix[][] = new int[3][3];
 
   // Set up objects to grab input from the file
   static File file;
@@ -47,10 +47,10 @@ public class Matrix { // begin class TuningCircuit
 
   // Default constructor for our object
   public Matrix() throws IOException {
-    file = new File(".\\matrices\\matrices.txt");
+    file = new File(".\\matrices\\matrix1.txt");
     fr = new FileReader(file);
     br = new BufferedReader(fr);
-    readArrayFromFile();
+    readMatrixFromFile();
   }
 
   // Argumented constructor for our object
@@ -58,30 +58,32 @@ public class Matrix { // begin class TuningCircuit
     file = new File(filename);
     fr = new FileReader(file);
     br = new BufferedReader(fr);
-    readArrayFromFile();
+    readMatrixFromFile();
   }
 
-  // Method to fill the array from file
-  public void readArrayFromFile() throws IOException {
+  // Method to fill the matrix from file
+  public void readMatrixFromFile() throws IOException {
     // Initialize our StringTokenizer
     StringTokenizer st;
     for (int i = 0; i < 3; i++) {
       // We must create a new string tokenizer for each new line we want to read
       st = new StringTokenizer(br.readLine());
       for (int j = 0; j < 3; j++) {
-        // Read the values of the array line by line
-        array[i][j] = Integer.parseInt(st.nextToken());
+        // Read the values of the matrix line by line
+        matrix[i][j] = Integer.parseInt(st.nextToken());
       }
-      // Print out the array
-      System.out.println(Arrays.deepToString(array));
     }
-    // br.flush();
+    // Print out the matrix
+    System.out.println("The matrix read from file is:");
+    printMatrixToConsole(matrix);
+
+    // Close buffered reader
     br.close();
   }
 
-  // Method to take the sum of two arrays
-  public static void sumOfArrays(int a[][], int b[][]) throws IOException {
-    // Initialize our resultant array
+  // Method to take the sum of two matricess
+  public static void sumOfMatrices(int a[][], int b[][]) throws IOException {
+    // Initialize our resultant matrix
     int c[][] = new int[3][3];
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
@@ -89,14 +91,15 @@ public class Matrix { // begin class TuningCircuit
         c[i][j] = a[i][j] + b[i][j];
       }
     }
-    // Print out the array
-    System.out.println(Arrays.deepToString(c));
-    printArrayToFile(c, ".\\matrices\\sumofmatrices.txt");
+    // Print out the matrix
+    System.out.println("The sum of the matrices is: ");
+    printMatrixToConsole(c);
+    printMatrixToFile(c, ".\\matrices\\sumofmatrices.txt");
   }
 
-  // Method to take the product of two arrays
-  public static void productOfArrays(int a[][], int b[][]) {
-    // Initialize our resultant array
+  // Method to take the product of two matrices
+  public static void productOfMatrices(int a[][], int b[][]) throws IOException {
+    // Initialize our resultant matrix
     int c[][] = new int[3][3];
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
@@ -105,13 +108,15 @@ public class Matrix { // begin class TuningCircuit
         c[i][j] = a[i][0]*b[0][j] + a[i][1]*b[1][j] + a[i][2]*b[2][j];
       }
     }
-    // Print out the array
-    System.out.println(Arrays.deepToString(c));
+    // Print out the matrix
+    System.out.println("The product of the matrices is: ");
+    printMatrixToConsole(c);
+    printMatrixToFile(c, ".\\matrices\\productofmatrices.txt");
   }
 
-  // Method to take the transpose of an array
-  public static void transposeOfArray(int a[][]) {
-    // Initialize our resultant array
+  // Method to take the transpose of a matrix
+  public static void transposeOfMatrix(int a[][]) throws IOException {
+    // Initialize our resultant matrix
     int c[][] = new int[3][3];
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
@@ -120,23 +125,17 @@ public class Matrix { // begin class TuningCircuit
         c[i][j] = a[j][i];
       }
     }
-    // Print out the array
-    System.out.println(Arrays.deepToString(c));
+    // Print out the matrix
+    System.out.println("The transpose of the matrix is: ");
+    printMatrixToConsole(c);
+    printMatrixToFile(c, ".\\matrices\\transposeofmatrix.txt");
   }
 
-  // Method to take the determinant of an array
-  public static void determinantOfArray(int a[][]) {
-    // Initialize our determinant
-    int determinant;
-    determinant =   a[0][0]*(a[1][1]*a[2][2]-a[1][2]*a[2][1])
-                  - a[0][1]*(a[1][0]*a[2][2]-a[1][2]*a[2][0])
-                  + a[0][2]*(a[1][0]*a[2][1]-a[1][1]*a[2][0]);
-    // Print out the array
-    System.out.println(determinant);
-  }
-
-  public static void cofactorOfArray(int a[][]) throws IOException {
-    // Initialize our resultant array
+  // Method to take the cafactor matrix of a matrix
+  public static void cofactorOfMatrix(int a[][]) throws IOException {
+    // Initialize our resultant matrix
+    // Calculates the cofactor matrix using a formula derived in the documentation
+    // See documentation, section X.X.X for more information
     int c[][] = new int[3][3];
     c[0][0] = a[1][1]*a[2][2] - a[1][2]*a[2][1];
     c[0][1] = -1*(a[1][0]*a[2][2] - a[1][2]*a[2][0]);
@@ -147,17 +146,55 @@ public class Matrix { // begin class TuningCircuit
     c[2][0] = a[0][1]*a[1][2] - a[0][2]*a[1][1];
     c[2][1] = -1*(a[0][0]*a[1][2] - a[0][2]*a[1][0]);
     c[2][2] = a[0][0]*a[1][1] - a[0][1]*a[1][0];
-    // Print out the array
-    printArrayToFile(c, ".\\matrices\\cofactormatrices.txt");
+
+    // Print out the matrix
+    System.out.println("The cofactor of the matrix is: ");
+    printMatrixToConsole(c);
+    printMatrixToFile(c, ".\\matrices\\cofactormatrix.txt");
   }
 
-  public static void printArrayToFile(int a[][], String filename) throws IOException {
+  // Method to take the determinant of a matrix
+  public static void determinantOfMatrix(int a[][]) throws IOException {
+    // Initialize our determinant
+    int determinant;
+    determinant =   a[0][0]*(a[1][1]*a[2][2]-a[1][2]*a[2][1])
+    - a[0][1]*(a[1][0]*a[2][2]-a[1][2]*a[2][0])
+    + a[0][2]*(a[1][0]*a[2][1]-a[1][1]*a[2][0]);
+
+    // Print out the matrix
+    System.out.println("The determinant of the matrix is: ");
+    System.out.println(determinant);
+    printIntegerToFile(determinant, ".\\matrices\\determinant.txt");
+  }
+
+  // Method to print a matrix to console
+  public static void printMatrixToConsole(int a[][]) {
+    for (int i = 0; i < 3; i++) {
+      System.out.println(a[i][0] + " " + a[i][1] + " " + a[i][2]);
+    }
+  }
+
+  // Method to print a matrix to file
+  public static void printMatrixToFile(int a[][], String filename) throws IOException {
     FileWriter tmpFile = new FileWriter(filename);
     BufferedWriter tempBW = new BufferedWriter(tmpFile);
     PrintWriter tempPW = new PrintWriter(tempBW);
     for (int i = 0; i < 3; i++) {
       tempPW.println(a[i][0] + " " + a[i][1] + " " + a[i][2]);
     }
+    // Close the output file streams
+    tempPW.flush();
+    tempPW.close();
+  }
+
+  // Method to print an integer to file
+  public static void printIntegerToFile(int a, String filename) throws IOException {
+    FileWriter tmpFile = new FileWriter(filename);
+    BufferedWriter tempBW = new BufferedWriter(tmpFile);
+    PrintWriter tempPW = new PrintWriter(tempBW);
+    tempPW.println(a);
+
+    // Close the output file streams
     tempPW.flush();
     tempPW.close();
   }

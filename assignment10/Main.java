42ee8776d9188149f6601633d0d66c3064fb3668
Connor Baker <connorbaker01@gmail.com>
2016-12-03 18:07:18 -0500

    assignment10: Main: v0.1b

--- assignment10/Main.java
+++ assignment10/Main.java
@@ -3,7 +3,7 @@
 *  File Name: Main.java
 *
 *  Author: Connor Baker
-*  Version: 0.1a
+*  Version: 0.1b
 *  Created: December 2, 2016
 *  Last Updated: December 2, 2016
 */
@@ -11,8 +11,11 @@
 // Declare package  name
 package assignment10;
 
+// Import necessary package(s)
+import java.io.IOException;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     // Create a new StackArray object
     StackArray newStackArray = new StackArray();
 

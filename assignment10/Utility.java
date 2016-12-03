723e93dce87d074bb3ec659132fc6e4060541a8c
Connor Baker <connorbaker01@gmail.com>
2016-12-03 18:06:31 -0500

    Assignment10: Utility: v0.1b

--- assignment10/Utility.java
+++ assignment10/Utility.java
@@ -3,7 +3,7 @@
 *  File Name: Utility.java
 *
 *  Author: Connor Baker
-*  Version: 0.1a
+*  Version: 0.1b
 *  Created: December 2, 2016
 *  Last Updated: December 2, 2016
 */
@@ -12,26 +12,35 @@
 package assignment10;
 
 // Import necessary package(s)
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Utility {
   // Create our shared variables
-  static  int lengthOfInput;
-  static  String input;
+  static int capacityOfStack;
+  static String input;
 
-  public void getInput() {
+  public void getInput() throws IOException {
     // Create a BufferedReader to grab user input
-    BufferedReader br = new BufferedReader(System.in);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     // Store captured user input as a String
     input = br.readLine();
 
     // Close our BufferedReader
     br.close();
+
+    // Debugging
+    System.out.println("Debugging: input was "+input);
   }
 
-  public void getLengthOfInput(String s) {
+  public void getLengthOfInput() {
     // Get the length of a String
-    lengthOfInput = input.length();
+    capacityOfStack = input.length();
+
+    // Debugging
+    System.out.println("Debugging: capacityOfStack is "+capacityOfStack);
   }
 
 }

37ec2b315a407dd928d5a319b29bd1067c98b5f7
Connor Baker <connorbaker01@gmail.com>
2016-12-03 22:55:06 -0500

    assignment10: InputUtility: v0.1a
    Rename and fix the previous Utility class.

--- assignment10/InputUtility.java
+++ assignment10/InputUtility.java
@@ -1,11 +1,11 @@
 /*
 *  Package Name: assignment10
-*  File Name: Utility.java
+*  File Name: InputUtility.java
 *
 *  Author: Connor Baker
-*  Version: 0.1b
-*  Created: December 2, 2016
-*  Last Updated: December 2, 2016
+*  Version: 0.1a
+*  Created: December 3, 2016
+*  Last Updated: December 3, 2016
 */
 
 // Declare package  name
@@ -16,31 +16,33 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public class Utility {
-  // Create our shared variables
-  static int capacityOfStack;
-  static String input;
-
-  public void getInput() throws IOException {
+public class InputUtility {
+  public static String getInput() throws IOException {
     // Create a BufferedReader to grab user input
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     // Store captured user input as a String
-    input = br.readLine();
+    String input = br.readLine();
 
     // Close our BufferedReader
     br.close();
 
     // Debugging
     System.out.println("Debugging: input was "+input);
+
+    // Return the input
+    return input;
   }
 
-  public void getLengthOfInput() {
+  public static int getLengthOfInput(String s) {
     // Get the length of a String
-    capacityOfStack = input.length();
+    int capacityOfStack = s.length();
 
     // Debugging
     System.out.println("Debugging: capacityOfStack is "+capacityOfStack);
+
+    // Return the length of the input
+    return capacityOfStack;
   }
 
 }

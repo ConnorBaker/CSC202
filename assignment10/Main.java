e80d8413d74976a1701c4260205737aa399e0667
Connor Baker <connorbaker01@gmail.com>
2016-12-04 13:45:38 -0500

    assignment10: Main: v0.1d
    Re-work what the program does with input. I think it’s better that the
    input is read in once, and then passed to all the constructors, rather
    than having a method call in each constructor to ask for input.
    
    However, Utility.getInput() is not set up to be called multiple times.
    The BufferedReader is closed at the end of the method call, and we
    don’t create a new one each time. As a result, if for some reason we
    didn’t pass the input into a constructor after having already called
    Utility.getInput(), the program will throw an error.

--- assignment10/Main.java
+++ assignment10/Main.java
@@ -3,7 +3,7 @@
 *  File Name: Main.java
 *
 *  Author: Connor Baker
-*  Version: 0.1c
+*  Version: 0.1d
 *  Created: December 2, 2016
 *  Last Updated: December 4, 2016
 */
@@ -16,12 +16,15 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) throws IOException {
+    // Get user input
+    String input = InputUtility.getInput();
+
     // Create a new StackArray object
-    StackArray newStackArray = new StackArray();
+    StackArray newStackArray = new StackArray(input);
     newStackArray.printStackOfChars();
 
     // Create a new SinglyLinkedList
-    SinglyLinkedList<Character> newSinglyLinkedList = new SinglyLinkedList<>();
+    // SinglyLinkedList<Character> newSinglyLinkedList = new SinglyLinkedList<>();
 
 
   }

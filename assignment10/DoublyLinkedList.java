a3861feef93ed05eedf687a4bb55892db8c576b9
Connor Baker <connorbaker01@gmail.com>
2016-12-04 13:43:30 -0500

    assignment10: DoublyLinkedList: v0.1c
    Create an argumented constructor.

--- assignment10/DoublyLinkedList.java
+++ assignment10/DoublyLinkedList.java
@@ -3,7 +3,7 @@
 *  File Name: DoublyLinkedList.java
 *
 *  Author: Connor Baker
-*  Version: 0.1b
+*  Version: 0.1c
 *  Created: December 3, 2016
 *  Last Updated: December 4, 2016
 *
@@ -17,15 +17,27 @@
 // Declare package  name
 package assignment10;
 
+// Import necessary package(s)
+import java.io.IOException;
+
 public class DoublyLinkedList<V> {
   // Declare our variables used in the class
   Node<V> head, tail;
   int size;
+  String input;
 
-  DoublyLinkedList() {
+  DoublyLinkedList() throws IOException {
     head = null;
     tail = null;
     size = 0;
+    input = InputUtility.getInput();
+  }
+
+  DoublyLinkedList(String input) {
+    head = null;
+    tail = null;
+    size = 0;
+    this.input = input;
   }
 
   public void addFirst(Node<V> Node) {

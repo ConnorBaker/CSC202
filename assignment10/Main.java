61959a8e544dd49bdc14e01722472a7c15b30888
Connor Baker <connorbaker01@gmail.com>
2016-12-05 00:53:17 -0500

    assignment10: Main: v0.2b
    Call DoublyLinkedList even though it’s basically a carbon copy of
    SinglyLinkedList at this point.

--- assignment10/Main.java
+++ assignment10/Main.java
@@ -3,7 +3,7 @@
 *  File Name: Main.java
 *
 *  Author: Connor Baker
-*  Version: 0.2a
+*  Version: 0.2b
 *  Created: December 2, 2016
 *  Last Updated: December 5, 2016
 */
@@ -27,6 +27,9 @@ public class Main {
 
     // Make the new singly linked list
     newSinglyLinkedList(input);
+
+    // Make the new doubly linked list
+    newDoublyLinkedList(input);
   }
 
   public static void newStackArray(String input) {
@@ -53,5 +56,19 @@ public class Main {
     // Print the new linked list
     System.out.println("The singly linked list returns: ");
     newSinglyLinkedList.print();
+  }
+
+  public static void newDoublyLinkedList(String input) {
+    // Create a new SinglyLinkedList
+    DoublyLinkedList newDoublyLinkedList = new DoublyLinkedList(input);
+
+    // Fill the new linked list
+    for (int i = 0; i < input.length(); i++) {
+      newDoublyLinkedList.add(input.charAt(i));
+    }
+
+    // Print the new linked list
+    System.out.println("The doubly linked list returns: ");
+    newDoublyLinkedList.print();
   }
 }

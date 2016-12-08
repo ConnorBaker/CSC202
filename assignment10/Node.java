983443f7ed4d03e11fd9b9b9b95f22fe094750af
Connor Baker <ConnorBaker01@gmail.com>
2016-12-07 23:01:06 -0500

    assignment10: Node: v0.2c
    Push missing comments

--- assignment10/Node.java
+++ assignment10/Node.java
@@ -3,9 +3,9 @@
 *  File Name: Node.java
 *
 *  Author: Connor Baker
-*  Version: 0.2b
+*  Version: 0.2c
 *  Created: December 3, 2016
-*  Last Updated: December 5, 2016
+*  Last Updated: December 7, 2016
 */
 
 // Declare package  name
@@ -17,34 +17,38 @@ public class Node {
   char element;
   Node next;
 
+  // Default constructor calls the argumented constructor
   Node() {
     this('\0', null);
   }
 
+  // Argumented constructor for the head
   Node(char element) {
     this(element, null);
   }
 
+  // Argumented constructor for actual nodes
   Node(char element, Node next) {
     this.element = element;
     this.next = next;
   }
 
+  // Set the link to the next node
   public void setNext(Node next) {
     this.next = next;
   }
 
-
+  // Get the link to the next node
   public Node getNext() {
     return next;
   }
 
-
+  // Set the character value of the node
   public void setChar(char element) {
     this.element = element;
   }
 
-
+  // Get the character value of the node
   public char getChar() {
     return element;
   }
